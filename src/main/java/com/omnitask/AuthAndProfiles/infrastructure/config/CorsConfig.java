9f49e68fc94aed0aa8d5a2570524fef7e7b95da4
Juan Pablo Caballero Castellanos <juan.ccastellanos@mail.escuelaing.edu.co>
2026-09-19 18:01:27 -0500

    feat: Añadiendo CORS para Front

--- src/main/java/com/omnitask/AuthAndProfiles/infrastructure/config/CorsConfig.java
+++ src/main/java/com/omnitask/AuthAndProfiles/infrastructure/config/CorsConfig.java
@@ -14,7 +14,7 @@ public class CorsConfig {
             @Override
             public void addCorsMappings(CorsRegistry registry) {
                 registry.addMapping("/api/v1/**")
-                        .allowedOrigins("http://localhost:5173")
+                        .allowedOrigins("http://localhost:5173", "https://taskit-frontend-omega.vercel.app")
                         .allowedMethods("GET", "POST", "PUT", "PATCH", "DELETE", "OPTIONS")
                         .allowedHeaders("*")
                         .allowCredentials(true);
